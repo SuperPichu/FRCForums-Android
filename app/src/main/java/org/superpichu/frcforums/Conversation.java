ffa312e110ff058f9fb7bfaa4ec109b00c918ab7
Chris Midkiff <chrismidkiff15@gmail.com>
2015-04-24 14:13:42 -0400

    Started implementing private messages

---
+++ app/src/main/java/org/superpichu/frcforums/Conversation.java
@@ -0,0 +1,10 @@
+package org.superpichu.frcforums;
+
+/**
+ * Created by chris on 4/24/15.
+ */
+public class Conversation {
+    public String id;
+    public String[] participants;
+    public int newCount;
+}
---
+++ app/src/main/java/org/superpichu/frcforums/Message.java
@@ -0,0 +1,13 @@
+package org.superpichu.frcforums;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by chris on 4/24/15.
+ */
+public class Message {
+    public String user;
+    public String body;
+    public String ConversationId;
+    public Bitmap icon;
+}
