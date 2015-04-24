e999bf1e81bd69fa0e6be417b7dc14e46f0a0fd9
Chris Midkiff <chrismidkiff15@gmail.com>
2015-04-24 14:18:45 -0400

    Clean-up

--- app/src/main/java/org/superpichu/frcforums/LoginActivity.java
+++ app/src/main/java/org/superpichu/frcforums/LoginActivity.java
@@ -18,13 +18,6 @@ import java.io.File;
  */
 public class LoginActivity extends Activity {
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
     // UI references.
     private EditText mEmailView;
     private EditText mPasswordView;
